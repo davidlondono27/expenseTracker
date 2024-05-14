16d601cd935178d6af1bfe85c337332b515c2cb2
David Londono <hlc.david327@gmail.com>
2024-05-14 18:07:11 -0500

    Add new models and customization for Transactions

---
+++ Expense Tracker/Expense Tracker/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Expense Tracker
+//
+//  Created by David Londono on 14/05/24.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+}
